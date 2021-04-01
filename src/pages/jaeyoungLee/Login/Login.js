a9627f9ba8b7e9cd0ea90b8311d46d6c52e7991c
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-01 20:18:40 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/jaeyoungLee/Login/Login.js
+++ src/pages/jaeyoungLee/Login/Login.js
@@ -3,9 +3,29 @@ import './Login.scss';
 import { withRouter } from 'react-router-dom';
 
 class LoginJaeyoung  extends React.Component {
-    goToMain = () => {
-        this.props.history.push('/main')
+    constructor () {
+        super();
+        this.state = {
+            target : '',
+            target2 : '',
+        };
     }
+    handleIdInput = (e) => {
+        this.setState({
+            target : e.target.value
+        })
+        console.log(e.target.value);
+    }
+    handlePsInput = (e) => {
+        this.setState({
+            target2 : e.target.value
+        })
+        console.log(e.target.value);
+    }
+    goToMain = () => {
+        this.props.history.push('/main-jaeyoung')
+    }
+
     render () {
         return (
         <div className="loginContainer">
@@ -13,8 +33,8 @@ class LoginJaeyoung  extends React.Component {
                 <h1>Instargram</h1>
             </div>
             <div className="loginInfo">
-                <input type="text" className="loginId" placeholder="전화번호, 사용자 이름 또는 이메일" />
-                <input type="password" className="loginPs" placeholder="비밀번호"/>
+                <input type="text" value={this.state.target} onChange={this.handleIdInput} className="loginId" placeholder="전화번호, 사용자 이름 또는 이메일" />
+                <input type="password" value={this.state.target2} onChange={this.handlePsInput} className="loginPs" placeholder="비밀번호"/>
                 <button className="loginBt" onClick={this.goToMain}>로그인</button>
             </div>
             <div className="forgetPassword">
