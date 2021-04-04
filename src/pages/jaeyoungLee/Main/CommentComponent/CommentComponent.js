fd6dbdb7d429e89cb9721fbe32a4481efbdf2d1c
이재영 <2JAEYOUNG@bagchaehoui-MacBookAir.local>
2021-04-04 21:05:07 +0900

    Add : Mission 4 - 댓글 컴포넌트화, props로 데이터 전달 구현

--- src/pages/jaeyoungLee/Main/CommentComponent/CommentComponent.js
+++ src/pages/jaeyoungLee/Main/CommentComponent/CommentComponent.js
@@ -2,12 +2,10 @@ import React from 'react';
 import './CommentComponent.scss';
 
 class CommentComponent extends React.Component {
-  
   render() {
-    console.log(this.props);
     return (
       <div className='CommentComponent'>
-        <li key={this.props.index} className="commentBoxList">{this.props.commentList}</li>
+        <li key={this.props.id} className="commentBoxList">{this.props.commentList}</li>
       </div>
     )
   }
